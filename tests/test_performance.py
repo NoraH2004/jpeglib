992a41ff401cf90d8a2f83763fa8d1cd60bc8579
Martin Benes <martinbenes1996@gmail.com>
2022-01-04 09:10:52 +0100

    test of performance (5x slower)

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -31,10 +31,16 @@ class TestPerformance(unittest.TestCase):
                 flags=['DO_FANCY_UPSAMPLING','DO_BLOCK_SMOOTHING']
             )
             stego.append(t_stego.stop())
+<<<<<<< HEAD
         # test it isn't more than 3x slower
         max_3x_slower = ttest_ind(np.array(pil)*3, stego, alternative='less')
         logging.info('performance %.2fs vs. %.2fs of PIL' % (np.mean(pil), np.mean(stego)))
         self.assertGreater(max_3x_slower.pvalue, .05)
+=======
+        # test it isn't more than 5x slower
+        max_5x_slower = ttest_ind(np.array(pil)*5, stego, alternative='less')
+        self.assertGreater(max_5x_slower.pvalue, .05)
+>>>>>>> 52ea8bd (test of performance (5x slower))
     
     def test_writing(self):
         pass
